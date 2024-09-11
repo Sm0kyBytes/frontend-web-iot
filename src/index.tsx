a1eea751b008c1935480b81939b5de7b26b9ff95
Athikhom_ra <athikhom_ra@outlook.com>
2024-09-11 09:06:06 +0700

    feat: create axios interceptor

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./context/authentication";
+import jwtInterceptor from "./utils/jwtInterceptor";
+
+jwtInterceptor();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
