1b5608babe6216eac3ff3c0fd3edeb8e520f7e5c
Athikhom_ra <athikhom_ra@outlook.com>
2024-09-12 16:36:13 +0700

    feat: add background style image to app

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,36 @@ import RegisterPage from "./pages/Register";
 import ManagePage from "./pages/Manage";
 import CreateDevicePage from "./pages/CreateDevise";
 import Nav from "./components/nav/Nav";
-
+import BgImage from "./public/images/bg-example-image.svg";
 const App: React.FC = () => {
   return (
     <Router>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/manage" element={<ManagePage />} />
-        <Route path="/manage/create" element={<CreateDevicePage />} />
-        {/* <Route path="/manage/edit" element={<ManagePage />} /> */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div style={{ display: "flex" }}>
+        <img
+          src={BgImage}
+          alt="background image"
+          style={{
+            position: "absolute",
+            zIndex: 0,
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            opacity: 0.5,
+          }}
+        />
+        <div style={{ zIndex: 10 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/manage" element={<ManagePage />} />
+            <Route path="/manage/create" element={<CreateDevicePage />} />
+            {/* <Route path="/manage/edit" element={<ManagePage />} /> */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
