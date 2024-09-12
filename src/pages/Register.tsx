cd741063b4f014fbd1b04a66be4fc6d94ee5f096
Athikhom_ra <athikhom_ra@outlook.com>
2024-09-13 00:30:19 +0700

    feat: create style and error display for register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,76 +1,134 @@
 import React, { useState } from "react";
 import { RegisterModel } from "../models/auth";
 import { useAuth } from "../context/authentication";
+import { useNavigate } from "react-router-dom";
+//import MUI
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const RegisterPage: React.FC = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const authContext = useAuth();
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const navigate = useNavigate();
+
+  const [messageError, setMessageError] = useState<string | null>(null);
+
+  const cardStyle = {
+    minWidth: 300,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    border: "1px solid #888888",
+    boxShadow: "5px 10px #888888",
+  };
+  function isValidEmail(email: string): boolean {
+    const filter = /^([a-zA-Z0-9_\.-]+)@([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})$/;
+    return filter.test(email);
+  }
+
+  function isValidPassword(password: string): boolean {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,15}$/;
+    return regex.test(password);
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    authContext?.registerUser(email, username, password);
+    setMessageError(null);
+    if (!isValidEmail(email)) {
+      setMessageError("Invalid email.");
+    } else if (!isValidPassword(password)) {
+      setMessageError(
+        "Password must contain both uppercase and lowercase letters, numbers, and be between 8 and 15 characters long."
+      );
+    } else {
+      const message = await authContext?.registerUser(
+        email,
+        username,
+        password
+      );
+      if (message === "User has been created successfully.") {
+        setMessageError(null);
+      } else if (message) {
+        setMessageError(message);
+      } else {
+        setMessageError(null);
+      }
+    }
+  };
+
+  const goToLoginPage = (event: React.MouseEvent<HTMLElement>) => {
+    navigate("/login");
   };
   return (
-    <div className="page-container">
-      <h1>Register Page</h1>
-      <div className="register-form-container">
-        <form className="register-form" onSubmit={handleSubmit}>
-          <h2>Register Form</h2>
-          <div className="input-container">
-            <label>
-              Username:
-              <input
-                id="username"
-                name="username"
-                type="text"
-                placeholder="Enter username here"
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setUsername(event.target.value);
-                }}
-                value={username}
-                required
-              />
-            </label>
+    <Card sx={cardStyle}>
+      <CardContent>
+        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
+          Register Form
+        </Typography>
+        <Box
+          component="form"
+          sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
+          // noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <div
+            className="input-container"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              required
+              label="Username"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setUsername(event.target.value);
+              }}
+              value={username}
+            />
+            <TextField
+              required
+              label="Email"
+              type="email"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setEmail(event.target.value);
+              }}
+              value={email}
+            />
+            <TextField
+              required
+              label="Password"
+              type="password"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setPassword(event.target.value);
+              }}
+              value={password}
+            />
+            <Button type="submit" fullWidth sx={{ fontWeight: 700 }}>
+              Submit
+            </Button>
           </div>
-          <div className="input-container">
-            <label>
-              Password:
-              <input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="Enter password here"
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setPassword(event.target.value);
-                }}
-                value={password}
-                required
-              />
-            </label>
-          </div>
-          <div className="input-container">
-            <label>
-              Email:
-              <input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Enter email here"
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setEmail(event.target.value);
-                }}
-                value={email}
-                required
-              />
-            </label>
-          </div>
-          <div className="form-actions">
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    </div>
+          {messageError ? (
+            <Typography sx={{ color: "red" }}>*{messageError}</Typography>
+          ) : null}
+        </Box>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={goToLoginPage}>
+          Already have account?
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
